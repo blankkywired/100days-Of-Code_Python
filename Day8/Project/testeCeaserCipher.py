e17f9a6abf8aac950c7c9d4fef8a47fdcd213ced
blankkywired <blankky@protonmail.com>
2025-02-22 12:43:20 -0300

    Modified files

--- Day8/Project/testeCeaserCipher.py
+++ Day8/Project/testeCeaserCipher.py
@@ -1,10 +1,11 @@
 import letters
 
 
-message = ""
+
 
 message = input('Digite uma mensagem para codificar: ')
-message = message.lower() #Transforma todo o conteudo da mensagem em lowercase
+message = message.lower()
+# Transforma todo o conteudo da mensagem em lowercase
 print(message)
 
 splitMensage = [i for i in message if i != ' '] #Cria uma lista contendo cada caractere da mensagem do usuario que nao seja esse caractere um

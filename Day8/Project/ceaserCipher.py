895c2e6d380232178c8a07fb7dae8e5f7b8ddcb6
blankkywired <blankky@protonmail.com>
2025-03-22 19:27:17 -0300

    modified files

--- Day8/Project/ceaserCipher.py
+++ Day8/Project/ceaserCipher.py
@@ -5,6 +5,7 @@ print(art.title)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+
 def ceaser(original_text, shift_amount, encode_decode):
     output_text = ""
     if encode_decode == "decode":

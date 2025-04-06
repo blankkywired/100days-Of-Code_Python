686ae1615f72a5607e4193b7e0c99541cf2eaf0c
blankkywired <blankky@protonmail.com>
2025-04-06 20:49:25 -0300

    Added files

---
+++ Day9/Notes/nestingNote.py
@@ -0,0 +1,34 @@
+#Nesting
+
+#Usando valores aninhados dentro de dicionarios:
+
+capitais_pais = {
+    'França': 'Paris',
+    'Alemanha': 'Berlim',
+    'Russia': 'Moscou',
+    'Portugal': 'Lisboa'
+}
+
+
+
+#Usando listas em dicionarios:
+cidades_visitados_por_paises = {
+    'França': ['Paris', 'Strasbourg', 'Bordeaux'],
+
+}
+
+cidades_alemanha = {
+    'Alemanha': 'Berlim',
+
+}
+cidades_visitados_por_paises2 = {
+    'França': ['Paris', 'Strasbourg', 'Bordeaux'],
+}
+
+print(cidades_visitados_por_paises2['França'][1]) # --- > Mostrando um elemento da lista da Key(frança) dentro do dicionario,  Output = Strasbourg
+
+nested_list = ["A", "B", ["C", "D"]]
+
+print(nested_list[2][1]) # ----> Exibindo um elemento de uma lista aninhada Output: "D"
+
+

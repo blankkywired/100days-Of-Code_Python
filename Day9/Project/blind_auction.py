0f2b9c0daf16ea3bdccd1edccab001ee7ae9bc5b
blankkywired <blankky@protonmail.com>
2025-04-18 20:17:59 -0300

    Added files

--- Day9/Project/blind_auction.py
+++ Day9/Project/blind_auction.py
@@ -8,20 +8,27 @@ start_code = True
 data_base = {
 
 }
+
+def most_expensive():
+
 while start_code:
-    name_user = input('What is your name?: ').lower()
+    name_user = input('What is your name?: ')
     bid_user = int(input('What is your bid?: $'))
     bidders_question = input("Are there any other bidders?: Type 'yes' or 'no'.\n").lower()
 
+    #Adding name and bid in the dict
     data_base[name_user] = bid_user
 
     if bidders_question == "no":
         start_code = False
 
-    #Limpar a tela do usuario no terminal
+
     elif bidders_question == "yes":
-        os.system('cls' if os.name == 'nt' else 'clear') #Rodar codigo no terminal powershell pelo pycharm
+        os.system('cls' if os.name == 'nt' else 'clear') #-Clean the screen after the user's input
+         
 
-
+#Mostrar os respectivos valores -- nome e quantia 
 for user in data_base:
     print(f'{user}: ${data_base[user]}')
+
+

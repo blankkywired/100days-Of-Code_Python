95d0263bbe4098f1b0f3a30c5d9c67aa3e7d3d6e
blankkywired <blankky@protonmail.com>
2025-04-19 07:53:46 -0300

    Added files

--- Day9/Project/blind_auction.py
+++ Day9/Project/blind_auction.py
@@ -5,22 +5,28 @@ print(draw.simbol)
 
 start_code = True
 
-data_base = {
-
-}
-
-def most_expensive():
-
+data_base = {}
+values_bid = []
+def most_expensive(value):
+    max_value = 0
+    for j in value:
+        if j > max_value:
+            max_value = j
+    return (f'The highest value is R${max_value}')
+    
 while start_code:
     name_user = input('What is your name?: ')
     bid_user = int(input('What is your bid?: $'))
     bidders_question = input("Are there any other bidders?: Type 'yes' or 'no'.\n").lower()
 
+    #Adding the bid for the list
+    values_bid += [bid_user]
     #Adding name and bid in the dict
     data_base[name_user] = bid_user
 
     if bidders_question == "no":
         start_code = False
+        print(most_expensive(value=values_bid))
 
 
     elif bidders_question == "yes":
@@ -28,6 +34,7 @@ while start_code:
          
 
 #Mostrar os respectivos valores -- nome e quantia 
+print("-" * 15 , "RESULT" , "-" * 20)
 for user in data_base:
     print(f'{user}: ${data_base[user]}')
 
